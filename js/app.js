157c50fcf90a6f977ad10a9092acceeb53ddd4e7
Naiem890 <solaiman.naiem890@gmail.com>
2022-03-10 23:45:34 +0600

    comment fix

--- js/app.js
+++ js/app.js
@@ -129,9 +129,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -142,6 +142,7 @@ const createPost = (post) => {
 };
 
 const showPosts = (posts) => {
+  console.log(posts);
   const productsContainer = document.getElementById("posts");
   productsContainer.innerHTML = "";
 
